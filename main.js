c0a4a111742552f4ae077c24974fd003e13a6491
Julio Silva <144964918+jcosilva@users.noreply.github.com>
2024-11-26 09:53:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const NameElement = document.querySelector('#NameElement');
+    const UsernameElement = document.querySelector('#UsernameElement');
+    const AvatarElement = document.querySelector('#AvatarElement');
+    const ReposElement = document.querySelector('#ReposElement');
+    const FollowersElement = document.querySelector('#FollowersElement');
+    const FollowingElement = document.querySelector('#FollowingElement');
+    const LinkElement = document.querySelector('#LinkElement');
+    const EndPoint = 'https://api.github.com/users/jcosilva';
+
+
+    fetch(EndPoint)
+        .then(function(answer) {
+            return answer.json();
+        })
+        .then(function(json) {
+        //Avatar do Usuário
+            AvatarElement.src = json.avatar_url;
+        //Nome do Usuário
+            if (json.name == null) {
+                NameElement.innerText = "Update GitHub Info";
+            } else {
+                NameElement.innerText = json.name;
+            }
+        //Username do Usuário
+            UsernameElement.innerText = json.login;
+        //Número de Repositórios do Usuário
+            ReposElement.innerText = json.public_repos;
+        //Número de seguidores do Usuário
+            FollowersElement.innerText = json.followers;
+        //Número de pessoas sendo seguindas pelo Usuário
+            FollowingElement.innerText = json.following;
+        //Link para a página do GitHub do Usuário
+            LinkElement.href = json.html_url
+        })
+})
