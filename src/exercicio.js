84bc797e4fc70c1e1d41175c9efda514b5b6e5dd
Julio Silva <144964918+jcosilva@users.noreply.github.com>
2024-11-28 10:46:23 -0500

    Arquivos para a resolução do exercicio de Typescript

---
+++ src/exercicio.js
@@ -0,0 +1,14 @@
+"use strict";
+//Exercicio 1: Função de Multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+const resultadoMultiplicacao = multiplicar(5, 13);
+console.log(resultadoMultiplicacao);
+console.log("-------------------------");
+//Exercicio 2: Função de Saudação
+function saudar(nome) {
+    return `Olá ${nome}`;
+}
+const saudacao = saudar("Maria");
+console.log(saudacao);
---
+++ src/exercicio.ts
@@ -0,0 +1,17 @@
+//Exercicio 1: Função de Multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+const resultadoMultiplicacao = multiplicar(5, 13);
+console.log(resultadoMultiplicacao);
+console.log("-------------------------");
+
+
+//Exercicio 2: Função de Saudação
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+const saudacao = saudar("Maria");
+console.log(saudacao);
